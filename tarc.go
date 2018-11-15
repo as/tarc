6b9072714b03c5e356884664b8a527228d1cca7a
as <as.utf8@gmail.com>
2018-11-14 19:26:35 -0800

    first

---
+++ tarc.go
@@ -0,0 +1,43 @@
+package lru
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+type cacheLinePad = [64]byte
+
+type Entry struct {
+	key, value string
+	time.Duration
+}
+
+type TARC struct {
+	x uint32
+	c [8]Entry
+	time.Duration
+	_ cacheLinePad
+}
+
+func (c *TARC) Put(key, value string) {
+	c.c[atomic.AddUint32(&c.x, 1)&7] = Entry{key, value, time.Duration(time.Now().UnixNano())}
+}
+
+func (c *TARC) Get(key string) (value string, ok bool) {
+	mask := uint32(len(c.c) - 1)
+	i := atomic.LoadUint32(&c.x) & mask
+	si := i
+	ei := (si + 1) & mask
+	for si != ei {
+		v := c.c[si]
+		if key == v.key {
+			if time.Duration(time.Now().UnixNano())-v.Duration > c.Duration {
+				return "", false
+			}
+			atomic.CompareAndSwapUint32(&c.x, i, si)
+			return v.value, true
+		}
+		si = (si - 1) & mask
+	}
+	return "", false
+}
---
+++ tarc_test.go
@@ -0,0 +1,85 @@
+package lru
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTARC(t *testing.T) {
+	c := TARC{Duration: time.Second / 2}
+	lo := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
+	hi := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I"}
+	i := 0
+	for i = range lo {
+		c.Put(fmt.Sprint(lo[i]), hi[i])
+		ch, ok := c.Get(lo[i])
+		if !ok || ch != hi[i] {
+			t.Fatalf("have %q, want %q", ch, hi[i])
+		}
+	}
+	if _, ok := c.Get("A"); ok {
+		t.Fatal("space: stale entry in cache")
+	}
+	c.Put("?", "!")
+	if ch, ok := c.Get("?"); !ok || ch != "!" {
+		t.Fatalf("have %q, want %q", ch, "!")
+	}
+	time.Sleep(time.Second)
+	if _, ok := c.Get("?"); ok {
+		t.Fatal("time: stale entry in cache")
+	}
+}
+func BenchmarkTARC(b *testing.B) {
+	c := TARC{Duration: time.Second / 2}
+	lo := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i"}
+	hi := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I"}
+	i := 0
+
+	b.Run("Put", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			if i > len(lo) {
+				i = 0
+			}
+			c.Put(lo[i], hi[i])
+		}
+	})
+	i = 0
+
+	b.Run("Get", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			if i > len(lo) {
+				i = 0
+			}
+			c.Get(lo[i])
+		}
+	})
+
+	b.Run("Parallel", func(b *testing.B) {
+		for _, cpu := range []int{0, 1, 2, 4} {
+			b.Run(fmt.Sprintf("%dWriters", cpu), func(b *testing.B) {
+				done := make(chan bool)
+				defer close(done)
+				for x := 0; x < cpu; x++ {
+					go func() {
+						for {
+							select {
+							case <-done:
+								return
+							default:
+								c.Put("x", "y")
+								c.Put("y", "x")
+							}
+						}
+					}()
+				}
+				b.RunParallel(func(pb *testing.PB) {
+					for pb.Next() {
+						c.Get("x")
+					}
+				})
+			})
+		}
+	})
+
+}
